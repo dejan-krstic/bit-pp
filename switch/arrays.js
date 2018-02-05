26fa8b05a5e7d0ace4d73f64e7f2d2212a6962eb
BIT Student <classroom@bgit.rs>
2018-02-05 17:50:06 +0100

    switch done

---
+++ switch/arrays.js
@@ -0,0 +1,26 @@
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+console.log(months[5], months[9], months[0]);
+
+var week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+console.log(week[6]);
+
+
+
+var numbers = [2, -4, 5, -2, -11];
+
+console.log(numbers[1], numbers[3], numbers[4]);
+
+
+
+var numbers1 = [5, 15, -5, 20, 12, 18, 72, 14, 9]
+
+
+console.log(numbers1[1], numbers1[4], numbers1[5], numbers1[6], numbers1[8]);
+
+
+var array1 = [[1, 2, 1, 24], [8, 11, 9, 4],[7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 11]];
+
+console.log(array1[2]);
+console.log(array1[2][1]);
