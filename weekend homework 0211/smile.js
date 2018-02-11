4faf092ca279902d0c00b8e0205b0f546bad0bc4
dejan-krstic <dejanmkrstic@gmail.com>
2018-02-11 12:33:55 +0100

    weekend homework with smile

--- weekend homework 0211/smile.js
+++ weekend homework 0211/smile.js
@@ -1,10 +1,13 @@
-function smile(n, lineWidth) {
-    var sqc = n * n / 4;
-    var fi = n + 1;
-    var l = '';
+
+
+function smile(faceSize, lineWidth) {
+    l = '';
+    n=faceSize; //faceSize and lineWidth are to long to manipulate with ease
+    sqc = n * n / 4;
+    fi = n + 1;
     if (lineWidth) fi = lineWidth;
-    var pnp = 0;
-    if (n % 2) pnp = 1;
+    pnp = 0;
+    //if (n % 2) pnp = 1; // for two types of centering
     for (i = 0; i <= n; i++) {
         sqa = (parseInt(n / 2) - i + pnp) * (parseInt(n / 2) - i + pnp);
         sqEyeI = (parseInt(n / 3) - i + pnp) * (parseInt(n / 3) - i + pnp);
@@ -13,7 +16,7 @@ function smile(n, lineWidth) {
             sqb = (parseInt(n / 2) - j + pnp) * (parseInt(n / 2) - j + pnp);
             sqEyeJa = (parseInt(n / 3.5) - j + pnp) * (parseInt(n / 3.5) - j + pnp);
             sqEyeJb = (parseInt(n / 1.4) - j + pnp) * (parseInt(n / 1.4) - j + pnp);
-            if ((sqa + sqb - fi < sqc && sqa + sqb + fi > sqc) || (sqa + sqb - fi < sqc / 2.5 && sqa + sqb + fi > sqc / 2.5 && i > n * 2 / 3) || (sqEyeI + sqEyeJb < n / 1.7) || (sqEyeI + sqEyeJa < n / 1.7 && sqEyeI + sqEyeJa > n / 5 && i > n / 3)) l += ' * ';
+            if ((sqa + sqb - fi < sqc && sqa + sqb + fi > sqc) || (sqa + sqb - fi < sqc / 2.5 && sqa + sqb + fi > sqc / 2.5 && i > n * 2 / 3) || (sqEyeI + sqEyeJb < fi / 1.7) || (sqEyeI + sqEyeJa < fi / 1.7 && sqEyeI + sqEyeJa > fi / 5 && i > n / 3)) l += '*  ';
             else l += '   ';
         }
         l += '\n';
@@ -21,4 +24,5 @@ function smile(n, lineWidth) {
     }
     return (l);
 }
-console.log(smile(49));
+console.log(smile(48,56));
+
