51e16813766ee7bbd1779d0adb485ef1918a5f03
dejan-krstic <dejanmkrstic@gmail.com>
2018-02-14 20:33:37 +0100

    ispravka

--- callback 0214/test 5.js
+++ callback 0214/test 5.js
@@ -1,19 +1,21 @@
-function f(a){
-    var k=0;
-        
-    return function() {
-            var b="";
-            for (i=0; i<a.length; i++)
-                {
-                    if (a[i]=='a') {b+='*'; k++;}
-                    else b+=a[i];
-                    
-            };
-            return(b);
+function f(a) {
+    var k = 0;
 
-        }; return(k);
+    return function () {
+        var b = "";
+        for (i = 0; i < a.length; i++) {
+            if (a[i] == 'a') { b += '*'; k++; }
+            else b += a[i];
+
+        };
+
+        console.log(b);
+        return k;
+
+    };
+  
 
 };
-// f("javascript")();
 
- console.log(f('javascript'),f('javascript')());
+
+console.log(f('javascript')());
