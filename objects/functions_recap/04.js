3160ce5fab5817f35de480bc124742e7beb7ce3d
dejan-krstic <dejanmkrstic@gmail.com>
2018-02-19 23:32:00 +0100

    late evening addon

--- objects/functions_recap/04.js
+++ objects/functions_recap/04.js
@@ -1,5 +1,7 @@
 console.log(function reduce(a){
     var c=a;
     c.sort(function(x,y){return x-y} );
-    return {smallestValue: c[0], last_index : a.lastIndexOf(6), as : a};
+    return {smallestValue: c[0], last_index : a.lastIndexOf(c[0]), givenArray : a};
 }([1,2,-1,3,1,6,1,6,-1,6,6,5,2]));
+
+//??? a[] shouldn't be sorted
